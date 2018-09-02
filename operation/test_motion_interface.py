a44c3e8065b2604b63f8621e8d4681bf1cefa4e9
Ian Huang <ian.huang@dyn-160-39-132-248.dyn.columbia.edu>
2018-09-02 13:16:20 -0400

    test methods for motion interface.

---
+++ operation/test_motion_interface.py
@@ -0,0 +1,33 @@
+"""
+@author: Ian Huang
+
+This is the unit test for the motion interface.
+"""
+
+
+import unittest
+
+class TestAngleCommander(unittest.TestCase):
+    def test_get_initial_state(self):
+        pass
+    def test_checkAngleLimits(self):
+        pass
+    def test_goTo(self):
+        pass
+    def test_getCubicSmoothing(self):
+        pass
+    def test_writeLine(self):
+        pass
+    def test_writePosTransLine(self):
+        pass
+    def test_writeImageLine(self):
+        pass
+    def test_writeVideoLine(self): 
+        pass
+    def logState(self):
+        pass
+    def imageCapture(self): 
+        pass
+
+if __name__=='__main__':
+    unittest.main()
